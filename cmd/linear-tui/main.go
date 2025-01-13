4f8672fa0ad4f389216dbea8ebde8bba2b834c62
Daniel <danniieelg@gmail.com>
2025-01-13 19:56:15 +0000

    Setup application

---
+++ cmd/linear-tui/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DanielGilchrist/linear-tui/internal/api"
+	"github.com/DanielGilchrist/linear-tui/internal/tui"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	// TODO: Retrieve from user and store it somewhere safe on their machine
+	apiToken := os.Getenv("LINEAR_API_TOKEN")
+
+	if apiToken == "" {
+		fmt.Println("Please set LINEAR_API_TOKEN environment variable")
+		os.Exit(1)
+	}
+
+	client := api.NewClient(apiToken)
+	model := tui.NewModel(client)
+	p := tea.NewProgram(model)
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
---
+++ internal/api/client.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const apiEndpoint = "https://api.linear.app/graphql"
+
+type Client struct {
+	httpClient *http.Client
+	apiToken   string
+}
+
+func NewClient(apiToken string) *Client {
+	return &Client{
+		httpClient: &http.Client{},
+		apiToken:   apiToken,
+	}
+}
+
+func (c *Client) GetTeamsWithIssues() (*TeamsResponse, error) {
+	query := `
+		query TeamsWithIssues {
+			teams {
+				nodes {
+					id
+					name
+					issues {
+						nodes {
+							id
+							title
+							description
+							sortOrder
+						}
+					}
+					issueCount
+				}
+			}
+		}
+	`
+
+	req := struct {
+		Query string `json:"query"`
+	}{Query: query}
+
+	var resp TeamsResponse
+	if err := c.makeRequest(req, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func (c *Client) makeRequest(req interface{}, resp interface{}) error {
+	jsonBytes, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("marshal request: %w", err)
+	}
+
+	httpReq, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return fmt.Errorf("create request: %w", err)
+	}
+
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Set("Authorization", c.apiToken)
+
+	httpResp, err := c.httpClient.Do(httpReq)
+	if err != nil {
+		return fmt.Errorf("do request: %w", err)
+	}
+	defer httpResp.Body.Close()
+
+	body, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return fmt.Errorf("read response: %w", err)
+	}
+
+	if err := json.Unmarshal(body, resp); err != nil {
+		return fmt.Errorf("unmarshal response: %w", err)
+	}
+
+	return nil
+}
