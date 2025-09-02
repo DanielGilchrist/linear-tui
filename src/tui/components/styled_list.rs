098b4d7f5c3bec5c3ef75944c263749a8f5bcea8
Daniel Gilchrist <danniieelg@gmail.com>
2025-09-02 19:38:09 +0100

    Allow StyledList to render a placeholder

--- src/tui/components/styled_list.rs
+++ src/tui/components/styled_list.rs
@@ -1,7 +1,7 @@
 use ratatui::{
     layout::Rect,
     style::{Color, Modifier, Style},
-    widgets::{Block, Borders, List, ListItem, ListState},
+    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
     Frame,
 };
 
@@ -10,6 +10,7 @@ pub struct StyledList<'a> {
     title: String,
     focused: bool,
     state: Option<&'a mut ListState>,
+    placeholder: Option<String>,
 }
 
 impl<'a> StyledList<'a> {
@@ -19,6 +20,7 @@ impl<'a> StyledList<'a> {
             title: title.to_string(),
             focused: false,
             state: None,
+            placeholder: None,
         }
     }
 
@@ -37,6 +39,11 @@ impl<'a> StyledList<'a> {
         self
     }
 
+    pub fn placeholder(mut self, placeholder: &str) -> Self {
+        self.placeholder = Some(placeholder.to_string());
+        self
+    }
+
     pub fn render(self, frame: &mut Frame, area: Rect) {
         let border_style = if self.focused {
             Style::default().fg(Color::Yellow)
@@ -44,13 +51,18 @@ impl<'a> StyledList<'a> {
             Style::default().fg(Color::Gray)
         };
 
+        let block = Block::default()
+            .title(self.title)
+            .borders(Borders::ALL)
+            .border_style(border_style);
+
+        if let Some(placeholder) = self.placeholder {
+            let placeholder = Paragraph::new(placeholder).block(block);
+            return frame.render_widget(placeholder, area);
+        }
+
         let list = List::new(self.items)
-            .block(
-                Block::default()
-                    .title(self.title)
-                    .borders(Borders::ALL)
-                    .border_style(border_style),
-            )
+            .block(block)
             .highlight_style(Style::default().add_modifier(Modifier::REVERSED));
 
         if let Some(state) = self.state {
